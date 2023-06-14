c35eecbd81ff7045a7895cfbc3ffef9811e0a556
Marcos <marcospegodesousa10@gmail.com>
2023-06-14 14:19:52 -0300

    14062023

--- src/main/java/com/othon/carvalho/model/app/Produto.java
+++ src/main/java/com/othon/carvalho/model/app/Produto.java
@@ -2,6 +2,7 @@ package com.othon.carvalho.model.app;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,16 +19,19 @@ public class Produto{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Long codprod;
-    private Long codauxiliar;
     private String descricao;
     private String unidade;
+    private Long codauxiliar;
     private BigDecimal precoatual;
+    private String dtalteracaopreco;
+    private BigDecimal precoanterior;
     private BigDecimal precopromocional;
     private String fimvigencia;
     private String produtopai;
     private Integer estoquecd;
     private Integer estoqueothon;
     private Integer estoquedispothon;
+    private Integer qtdiassemvenda;
     private Integer qtachegar;
     private Integer giromes;
     private Integer qtvendida3meses;
